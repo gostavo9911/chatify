2a9d129c1959432e8c469082508ce649a688a820
ihab aghougali <37028449+gostavo9911@users.noreply.github.com>
2023-11-09 17:30:55 +0100

    Update listItem.blade.php
    changed strlen to mb_strlen to support multibyte characters such as Arabic names

--- src/views/layouts/listItem.blade.php
+++ src/views/layouts/listItem.blade.php
@@ -37,7 +37,7 @@ $lastMessageBody = strlen($lastMessageBody) > 30 ? mb_substr($lastMessageBody, 0
         {{-- center side --}}
         <td>
         <p data-id="{{ $user->id }}" data-type="user">
-            {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }}
+            {{ mb_strlen($user->name) > 12 ? trim(mb_substr($user->name, 0, 12)).'..' : $user->name }}
             <span class="contact-item-time" data-time="{{$lastMessage->created_at}}">{{ $lastMessage->timeAgo }}</span></p>
         <span>
             {{-- Last Message user indicator --}}
